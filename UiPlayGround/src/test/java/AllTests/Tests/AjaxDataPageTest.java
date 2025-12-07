a36f703d4325ae8e0738eaa3213732535351374c
csengerthecoder <191019948+csengerthecoder@users.noreply.github.com>
2025-12-07 15:16:55 +0100

    created new ajaxdata

---
+++ UiPlayGround/src/test/java/AllTests/Tests/AjaxDataPageTest.java
@@ -0,0 +1,16 @@
+package AllTests.Tests;
+
+import AllTests.Base.BaseTest;
+import Pages.AjaxDataPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AjaxDataPageTest extends BaseTest {
+
+    @Test
+    public void AjaxDataButtonTest() {
+        AjaxDataPage ajaxDataPage = mainPage.openAjaxDataPage();
+        boolean isPElementClicked = ajaxDataPage.isPElementClicked();
+        Assertions.assertTrue(isPElementClicked);
+    }
+}
